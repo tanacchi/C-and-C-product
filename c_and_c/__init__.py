898019b24bb94bdf0a80b86a2834285111000426
tanacchi <q111026d@mail.kyutech.jp>
2020-09-09 10:12:34 +0900

    Modify to register new user to database

--- c_and_c/__init__.py
+++ c_and_c/__init__.py
@@ -1,5 +1,16 @@
+# Setup for Flask App
+import os
 from flask import Flask
 
 app = Flask(__name__)
+# app.secret_key = os.environ['SECRET_KEY']
+
+# Setup for Database
+from flask_sqlalchemy import SQLAlchemy
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///local.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 import c_and_c.views
--- c_and_c/views.py
+++ c_and_c/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
-from c_and_c import app
+from c_and_c import app, db
+from c_and_c.models import User
 from flask import (
     render_template, request,
     abort, redirect, url_for,
@@ -8,12 +9,17 @@ from flask import (
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('users/create.html')
     else:
-        print("User creation.")
+        new_user = User()
+        new_user.name = request.form.get("name")
+        new_user.type = request.form.get("type")
+        db.session.add(new_user)
+        db.session.commit()
         return render_template('index.html')
