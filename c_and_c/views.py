30ebb5dec424c34f1e319dd7ebfe5cf50965ce5a
tanacchi <q111026d@mail.kyutech.jp>
2020-09-09 09:36:05 +0900

    Add page of signup

--- c_and_c/views.py
+++ c_and_c/views.py
@@ -1,6 +1,19 @@
 from flask import render_template
 from c_and_c import app
+from flask import (
+    render_template, request,
+    abort, redirect, url_for,
+    flash
+)
 
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('users/create.html')
+    else:
+        print("User creation.")
+        return render_template('index.html')
