e3138eccd30399359fcb154465b2eb0a23fbce35
tanacchi <q111026d@mail.kyutech.jp>
2020-09-10 10:14:43 +0900

    Associate company and briefing (many-to-many)

--- c_and_c/models.py
+++ c_and_c/models.py
@@ -14,9 +14,15 @@ class History(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=False)
 
 
+company_briefing_table = db.Table('company_briefing_table',
+    db.Column('briefing_id', db.Integer, db.ForeignKey('briefing.id'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+)
+
 class Briefing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(1000), nullable=False)
+    participants = db.relationship('User', backref='briefings', secondary=company_briefing_table, lazy=True)
 
 
 class Lecture(db.Model):
